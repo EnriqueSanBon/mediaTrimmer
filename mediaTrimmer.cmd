797748551fd65152a4d4cb7b1fb976be2189888f
EnriqueSanBon <enriquesanchezbonet@gmail.com>
2018-12-31 01:59:49 +0100

    Duration of each part now can be selected

--- mediaTrimmer.cmd
+++ mediaTrimmer.cmd
@@ -1,23 +1,38 @@
 @echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
+setlocal EnableDelayedExpansion
+
+
 echo Comienza el Cortado del video
 ::ffmpeg -i the-simpsons-s01e04-esp-hdtv.avi -c copy -ss 00:05:00 -t 00:00:20 outfile.mp4
 pause
 
-@echo off 
-set /a count = 1
+set /a count=0
 set extension=.mp4
-set list=00:05:00 00:06:00 00:07:00 00:08:00 
-(for %%a in (%list%) do ( 
+set list=00:05:00 00:06:00 00:07:00 00:08:00
+set durations=00:00:05 00:00:10 00:00:15 00:00:20
+set nameInput=the-simpsons-s01e04-esp-hdtv.avi
+
+(for %%b in (%durations%) do (
+	set durations[!count!]=%%b
+	set /a count += 1
+))
+
+set /a count=0
+(for %%a in (%list%) do (
 	echo Empieza bucle
-	echo %%a 
-	echo %count%
+	echo %%a
+	echo !count!
+	call set "aux=%%durations[!count!]%%"
+	echo !aux!
+
 	echo %extension%
-	SET output=!count!%extension%
+	SET output=%nameInput%!count!%extension%
     echo !output!
-  	ffmpeg -i the-simpsons-s01e04-esp-hdtv.avi -c copy -ss %%a -t 00:00:20 !output!
+  	echo ffmpeg -i %nameInput% -c copy -ss %%a !aux! -t !output!
+  	ffmpeg -i %nameInput% -c copy -ss %%a -t !aux! !output!
 	set /a count += 1
 ))
 pause
+endlocal
 
 exit
