1d018dfc5ad743cf8a0cf8c1855be95116839324
EnriqueFinweg <enrique.sanchez@finweg.com>
2022-03-18 01:00:36 +0100

    Now you can specify video path by argument

--- mediaTrimmer.cmd
+++ mediaTrimmer.cmd
@@ -3,14 +3,19 @@ setlocal EnableDelayedExpansion
 
 
 echo Comienza el Cortado del video
+echo The current directory is %CD%
+@echo off
+set /p FILEPATH="Absolute path to video file: "
+echo The video path is: %FILEPATH%
 ::ffmpeg -i the-simpsons-s01e04-esp-hdtv.avi -c copy -ss 00:05:00 -t 00:00:20 outfile.mp4
 pause
 
 set /a count=0
 set extension=.mp4
-set list=00:05:00 00:06:00 00:07:00 00:08:00
-set durations=00:00:05 00:00:10 00:00:15 00:00:20
-set nameInput=the-simpsons-s01e04-esp-hdtv.avi
+set list=00:01:00 00:01:30 
+set durations=00:00:05 00:00:10 
+:::set nameInput=C:\FFmpeg\video.avi
+set nameInput=%FILEPATH%
 
 (for %%b in (%durations%) do (
 	set durations[!count!]=%%b
